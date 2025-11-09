7fb0aee32d4d3307022a8368bd692a267266468e
yhyoon1004 <yhyoon1004@naver.com>
2025-11-09 17:36:50 +0900

    네이버 지도 이슈 대응 config 수정

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
 import type { NextConfig } from "next";
 
-module.exports = { output: 'standalone' };
-
 const nextConfig: NextConfig = {
     output: "standalone",
     env: {
         NEXT_PUBLIC_NAVER_CLIENT_ID: "belzz8n1ww",
     },
-  /* config options here */
   reactStrictMode: true,
 };
 
