f309e7d3f4916e1d1d85fc7bc2fd7675358083dd
yhyoon1004 <yhyoon1004@naver.com>
2025-11-13 16:54:54 +0900

    아이폰 크롬 브라우져 파비콘 이슈 대응

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ ssl 인증서 : 시큐어사인
 동작 : nginx + docker  
   
 개발 스택 : typescirpt + nextjs
+
+  
+todo list  
+- 구글 애널리틱스 추가
+- search console 태그설정
+- seo 처리
+- 페이지 리퀴드 글라스 박스 스타일링 처리
+- 네이버 맵 api설정 추가
--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -17,9 +17,9 @@ const Layout = ({children}: { children: ReactNode }) => {
                 <link rel="icon" href="/favicon.ico"/>
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
                 <link rel="apple-touch-icon" type="image/png" sizes="192x192" href="/favicon-192x192.png"/>
                 <link rel="apple-touch-icon" type="image/png" sizes="512x512" href="/favicon-512x512.png"/>
-                <link rel="apple-touch-icon" href="/apple-touch-icon.png"/>
                 <link rel="manifest" href="/manifest.json"/>
                 <meta name="theme-color" content="#ffffff"/>
             </Head>
