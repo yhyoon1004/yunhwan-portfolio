40b2c28b254db490bb5b42e7a9accdab74d89116
yhyoon1004 <yhyoon1004@naver.com>
2025-11-15 22:10:45 +0900

    리퀴드 글래스 sx 추가

---
+++ mui-custom/liquid-glass/liquidGlassSx.ts
@@ -0,0 +1,16 @@
+import { alpha, type Theme } from '@mui/material/styles';
+import type { SystemStyleObject } from '@mui/system';
+
+export const liquidGlassSx = (theme: Theme): SystemStyleObject<Theme>  => ({
+    borderRadius: `calc(${theme.shape.borderRadius}px + 8px)`,
+    backdropFilter: 'blur(24px)',
+    border: '1px solid',
+    borderColor: (theme.vars || theme).palette.divider,
+    backgroundColor: theme.vars
+        ? `rgba(${theme.vars.palette.background.defaultChannel} / 0.4)`
+        : alpha(theme.palette.background.default, 0.4),
+    boxShadow: (theme.vars || theme).shadows[1],
+    padding: '8px 12px',
+    // 스크롤 영역일 때도 모양 유지
+    overflow: 'hidden',
+});
